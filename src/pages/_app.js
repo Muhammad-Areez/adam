c183541c2886d421901b33e2d378a23db0e04eb7
Ahmed Shaikh <muhammadyousuf@Admins-MacBook-Pro-5.local>
2024-12-18 00:35:48 +0500

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
-import '../public/assets/scss/CreateNewPass.scss';
-import '../public/assets/scss/PasswordRecovery.scss';
-import '../public/assets/scss/Reset.scss';
-import '../public/assets/scss/checkEmail.scss';
-import '../public/assets/scss/login.scss';
-import '../public/assets/scss/completeReset.scss';
+import '../../public/assets/scss/CreateNewPass.scss';
+import '../../public/assets/scss/PasswordRecovery.scss';
+import '../../public/assets/scss/Reset.scss';
+import '../../public/assets/scss/checkEmail.scss';
+import '../../public/assets/scss/login.scss';
+import '../../public/assets/scss/completeReset.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
